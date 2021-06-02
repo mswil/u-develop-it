c26d884da27dad8cdd5ab1eceec35c4ef143fe2a
Molly Wilson <molly_wilson1@outlook.com>
2021-06-02 13:31:29 -0500

    Parties API calls and update candidates' party API calls

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+// Candidates API calls
+// ======================================================================================
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
     const sql = `SELECT candidates.*, parties.name 
@@ -40,7 +42,7 @@ app.get('/api/candidates', (req, res) => {
     });
 });
 
-// Get a single candidate
+// GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
     const sql = `SELECT candidates.*, parties.name 
              AS party_name 
@@ -62,7 +64,7 @@ app.get('/api/candidate/:id', (req, res) => {
     });
 });
 
-// Delete a candidate
+// DELETE a candidate
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
@@ -84,7 +86,7 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
 });
 
-// Create a candidate
+// POST a candidate
 app.post('/api/candidate', ({ body }, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
     if (errors) {
@@ -107,11 +109,101 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `UPDATE candidates SET party_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            // check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+// ======================================================================================
+
+// Parties API calls
+// ======================================================================================
+// GET all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// GET a single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// DELETE a party
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            // checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+// ======================================================================================
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
+// Listener
+// ======================================================================================
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
